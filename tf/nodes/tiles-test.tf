7280604d318ea01186bf7247d78cfdad33582e91
symmatree <symmetry@pobox.com>
2025-11-01 14:17:16 -0400

    Try to bootstrap (#19)

--- tf/nodes/tiles-test.tf
+++ tf/nodes/tiles-test.tf
@@ -3,7 +3,8 @@ module "tiles-test" {
   source              = "../modules/talos-cluster"
   proxmox_storage_iso = var.proxmox_storage_iso
   cluster_name        = "tiles-test"
-  start_vms           = true # Start test VMs
+  start_vms           = true
+  run_bootstrap       = true
   onepassword_vault   = data.onepassword_vault.tf_secrets.uuid
   talos = {
     version   = var.talos_version
