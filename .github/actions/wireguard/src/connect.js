7d65bc25f7156c1407b1d227eef97598003aed96
Seth Porter <symmetry@pobox.com>
2025-11-16 12:25:19 -0500

    Try different wireguard approach

--- .github/actions/wireguard/src/connect.js
+++ .github/actions/wireguard/src/connect.js
@@ -4,20 +4,36 @@ import * as io from "@actions/io";
 import * as fs from "fs";
 
 try {
-  const vpnConfig = core.getInput("vpn_config");
-  core.info(`Wireguard config: ${vpnConfig}`);
+  // const vpnConfig = core.getInput("vpn_config");
+  // core.info(`Wireguard config: ${vpnConfig}`);
   const confDir = core.getInput("conf_dir");
   core.info(`Wireguard config directory: ${confDir}`);
+  const privateKey = core.getInput("private_key");
+  const publicKey = core.getInput("public_key");
+  core.info(`Wireguard public key: ${publicKey}`);
+  const endpoint = core.getInput("endpoint");
+  core.info(`Wireguard endpoint: ${endpoint}`);
+  const allowedIps = core.getInput("allowed_ips");
+  core.info(`Wireguard allowed IPs: ${allowedIps}`);
+  const clientIp = core.getInput("client_ip");
+  core.info(`Wireguard client IP: ${clientIp}`);
+  const gatewayIp = core.getInput("gateway_ip");
+  core.info(`Wireguard gateway IP: ${gatewayIp}`);
 
   core.info(`Installing wireguard`);
   await exec.exec('sudo', ['apt-get', '-y',
     '-qq', '-o', 'Dpkg::Progress-Fancy="0"', '-o', 'APT::Color="0"', '-o', 'Dpkg::Use-Pty="0"',
-    'install', 'wireguard-tools']);
+    'install', 'wireguard', 'wireguard-tools']);
   await io.mkdirP(confDir);
-  fs.writeFileSync(`${confDir}/wg0.conf`, vpnConfig, { mode: 0o600 });
+  fs.writeFileSync(`${confDir}/privatekey`, privateKey, { mode: 0o600 });
 
   core.info(`Connecting`);
-  await exec.exec('sudo', ['wg-quick', 'up', `${confDir}/wg0.conf`]);
+  // await exec.exec('sudo', ['wg-quick', 'up', `${confDir}/wg0.conf`]);
+
+  await exec.exec('sudo', ['ip', 'link', 'add', 'dev', 'wg0', 'type', 'wireguard']);
+  await exec.exec('sudo', ['ip', 'address', 'add', 'dev', 'wg0', clientIp, 'peer', gatewayIp]);
+  await exec.exec('sudo', ['wg', 'set', 'wg0', 'private-key', `${confDir}/privatekey`, 'peer', publicKey, 'allowed-ips', allowedIps, 'endpoint', endpoint]);
+  await exec.exec('sudo', ['ip', 'link', 'set', 'up', 'dev', 'wg0']);
   core.info(`Connected`);
 } catch (error) {
   core.setFailed(error.message);
--- .github/actions/wireguard/src/disconnect.js
+++ .github/actions/wireguard/src/disconnect.js
@@ -4,7 +4,8 @@ import * as exec from "@actions/exec";
 try {
   core.info(`Disconnecting`);
   const confDir = core.getInput("conf_dir");
-  await exec.exec('sudo', ['wg-quick', 'down', `${confDir}/wg0.conf`]);
+  // await exec.exec('sudo', ['wg-quick', 'down', `${confDir}/wg0.conf`]);
+  await exec.exec('sudo', ['ip', 'link', 'delete', 'dev', 'wg0']);
   core.info(`Disconnected`);
 } catch (error) {
   core.setFailed(error.message);
