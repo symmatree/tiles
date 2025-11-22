7f3fede3b62a0d316a30b90603868e974a1bc854
symmatree <symmetry@pobox.com>
2025-11-22 17:25:10 -0500

    Log versions and files to try to understand build problem (#40)
    * Log versions and files to try to understand build problem
    
    * Update helm locally
    
    * Try to kick cilium
    
    * Try to kick cilium
    
    * Stronger helm refresh
    
    * Try a more legal zone name
    
    * Okay try helm 4.0.0
    
    * Added template directory

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 targetRevision=main
 cluster_name=placeholder
@@ -7,14 +7,19 @@ cluster_id=666
 pod_cidr=placeholder
 vault_name=placeholder
 
+echo "::notice title=Helm::$(helm version)"
+
 for chart in charts/*; do
 	if [[ ! -f "$chart/Chart.yaml" ]]; then
 		continue
 	fi
 	name=$(basename "$chart")
+	echo "::group::Linting $name at $chart"
 	pushd "$chart"
 	# We do NOT dep update here because we might have cached it.
-	helm lint --strict
+	ls -lR
+	set -x
+	helm lint --strict .
 
 	helm template "${name}" . --namespace "${name}" \
 		--skip-crds \
@@ -24,6 +29,8 @@ for chart in charts/*; do
 		--set "pod_cidr=$pod_cidr" \
 		--set "vault_name=$vault_name" \
 		>rendered.yaml
+	set +x
 	#	| kubectl diff --server-side -f-
 	popd
+	echo "::endgroup::"
 done
