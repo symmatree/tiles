271b50ee0497721cc2903d145259a2506a351ec6
Seth Porter <symmetry@pobox.com>
2025-11-27 13:36:32 -0500

    Use actual types that exist

--- tf/modules/k8s-cluster/dns.tf
+++ tf/modules/k8s-cluster/dns.tf
@@ -51,12 +51,13 @@ data "cloudflare_zone" "parent_zone" {
 }
 
 # Create NS records in Cloudflare to delegate the subdomain to Google Cloud DNS
-resource "cloudflare_record" "delegation" {
+resource "cloudflare_dns_record" "delegation" {
   count = length(module.dns-public-zone.name_servers)
 
   zone_id = data.cloudflare_zone.parent_zone.id
   name    = var.cluster_name
   type    = "NS"
   ttl     = 300
-  value   = module.dns-public-zone.name_servers[count.index]
+  content = module.dns-public-zone.name_servers[count.index]
+  comment = "Managed by Terraform"
 }
