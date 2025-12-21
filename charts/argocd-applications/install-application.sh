a3703c3b0f5d76d4ddfdc0d8c07b0abc13a52868
symmatree <symmetry@pobox.com>
2025-12-21 12:58:09 -0500

    Fix cwd assumption in argocd-applications/install-application.sh (#147)

--- charts/argocd-applications/install-application.sh
+++ charts/argocd-applications/install-application.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 echo "::group::Install ArgoCD Applications"
-envsubst <application.yaml.tmpl | kubectl apply --server-side --force-conflicts -f-
+envsubst <charts/argocd-applications/application.yaml.tmpl | kubectl apply --server-side --force-conflicts -f-
 echo "::endgroup::"
