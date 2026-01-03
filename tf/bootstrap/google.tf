9bf01e0a47977fc854fe5b606a742867d492845e
symmatree <symmetry@pobox.com>
2026-01-03 18:15:26 -0500

    get rid of red herring SA (#243)

--- tf/bootstrap/google.tf
+++ tf/bootstrap/google.tf
@@ -26,51 +26,6 @@ resource "google_project_iam_member" "self-iam-admin" {
   member  = "user:${var.gcp_owner_email}"
 }
 
-resource "google_service_account" "tiles-tf" {
-  account_id   = "tiles-tf-sa"
-  display_name = "Tiles TF Service Account"
-}
-
-resource "google_service_account_iam_member" "self_impersonate" {
-  service_account_id = google_service_account.tiles-tf.id
-  role               = "roles/iam.serviceAccountTokenCreator"
-  member             = "serviceAccount:${google_service_account.tiles-tf.email}"
-}
-
-resource "google_project_iam_member" "tiles_tf_editor" {
-  project = var.seed_project_id
-  role    = "roles/editor"
-  member  = "serviceAccount:${google_service_account.tiles-tf.email}"
-}
-
-resource "google_project_iam_member" "tiles_tf_storage_admin" {
-  project = var.seed_project_id
-  role    = "roles/storage.admin"
-  member  = "serviceAccount:${google_service_account.tiles-tf.email}"
-}
-
-resource "google_project_iam_member" "tiles_tf_dns_admin" {
-  project = var.seed_project_id
-  role    = "roles/dns.admin"
-  member  = "serviceAccount:${google_service_account.tiles-tf.email}"
-  # Note: This role includes dns.managedZones.setIamPolicy permission needed
-  # to manage IAM policies on DNS managed zones. If you encounter 403 errors
-  # when setting IAM policies on managed zones, ensure permissions have propagated
-  # (may take a few minutes) or grant roles/dns.admin directly on the managed zone resource.
-}
-
-resource "google_project_iam_member" "tiles_tf_project_iam_admin" {
-  project = var.seed_project_id
-  role    = "roles/resourcemanager.projectIamAdmin"
-  member  = "serviceAccount:${google_service_account.tiles-tf.email}"
-}
-
-resource "google_project_iam_member" "tiles_tf_iam_security_admin" {
-  project = var.seed_project_id
-  role    = "roles/iam.securityAdmin"
-  member  = "serviceAccount:${google_service_account.tiles-tf.email}"
-}
-
 # Enable required APIs
 resource "google_project_service" "kms_api" {
   project = var.seed_project_id
@@ -102,13 +57,6 @@ resource "google_project_service" "seed_service_usage_api" {
   disable_on_destroy = false
 }
 
-
-
-output "gcp_tiles_tf_sa_email" {
-  description = "The email of the Tiles TF service account."
-  value       = google_service_account.tiles-tf.email
-}
-
 module "gcp_state_bucket" {
   source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
   version = ">= 12.0"
@@ -120,11 +68,12 @@ module "gcp_state_bucket" {
   iam_members = [
     {
       role   = "roles/storage.objectUser"
-      member = "serviceAccount:${google_service_account.tiles-tf.email}"
+      member = "serviceAccount:${google_service_account.tiles_terraform_oidc.email}"
     },
     {
       role   = "roles/storage.admin"
       member = "user:${var.gcp_owner_email}"
     },
   ]
+  depends_on = [google_service_account.tiles_terraform_oidc]
 }
